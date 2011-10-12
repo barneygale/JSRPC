a7b0fb798a24c6d1395cdaee82c5340fc771c18e
Barney Gale <barney.gale@gmail.com>
2011-10-12 15:11:24 +0100

    Fix hanging on Ctrl-C

--- demo.py
+++ demo.py
@@ -8,31 +8,31 @@ try:
 
     js = jsrpc.JSRPC(port=8080, http_root='demo_www')
 
+    #Sync
     print "### Testing synchronous"
     print "# Variable access"
     print "    -> window.location.href: %s" % js.sync.window.location.href
     print "# Function call"
     print "    -> window.prompt(): %s" % js.sync.window.prompt('Enter some data...')
 
-
+    #Async
     def location(value):
         print "    -> window.location.href: %s" % value
-
     def prompt(value):
         print "    -> window.prompt(): %s" % value
-
-
     print "### Testing asynchronous"
     print "# Variable access"
     js.async(location).window.location.href.flush() #Note that flush() is necessary for async variable access
     print "# Function call"
     js.async(prompt).window.prompt('Enter some data...')
 
+    #Async no return
     print "### Testing asynchronous no-return"
     print "# Variable access (lulwut)"
     js.async.window.location.href.flush()
     print "# Function call"
     js.async.window.prompt('Enter some data...')
     time.sleep(10000)
-except Exception, e:
+
+except KeyboardInterrupt, e:
     js.server.shutdown()
